d2d0298323bb58d9f352f9043fabf65714777a21
Andy <landywl4@gmail.com>
2023-02-16 11:57:47 -0800

    Added lines to branches and child nodes

--- components/TreeNode/TreeNode.js
+++ components/TreeNode/TreeNode.js
@@ -1,5 +1,5 @@
 import { css } from "@emotion/css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import TreeNodeAddChild from "./TreeNodeAddChild";
 
@@ -7,13 +7,52 @@ export default function TreeNode(props) {
   const { nodeData, curNode } = props;
   const [showAddChildForm, setShowAddChildForm] = useState(false);
   const currentNode = nodeData.find((node) => node._id === curNode);
+  const currentNodeRef = useRef();
+  const nodeChildRef = useRef();
+  const nodeBranchRef = useRef();
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const currentCoordinates = currentNodeRef?.current?.getBoundingClientRect();
+    if (!canvasRef?.current) return;
+    if (nodeChildRef.current) {
+      const childCoordinates =
+        nodeChildRef.current.firstChild.firstChild.getBoundingClientRect();
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.strokeStyle = "blue";
+      ctx.moveTo(0, 50);
+      ctx.lineTo(0, childCoordinates.top - currentCoordinates.bottom + 50);
+      ctx.stroke();
+    }
+    if (nodeBranchRef.current) {
+      const nodeBranchCoordinates =
+        nodeBranchRef.current.firstChild.firstChild.getBoundingClientRect();
+      console.log(nodeBranchCoordinates);
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.strokeStyle = "blue";
+      ctx.setLineDash([4, 4]);
+      ctx.lineWidth = 1;
+      const endX = nodeBranchCoordinates.right - currentCoordinates.right;
+      const endY =
+        nodeBranchCoordinates.top +
+        currentCoordinates.height / 2 -
+        currentCoordinates.bottom;
+      ctx.moveTo(90, 0);
+      ctx.bezierCurveTo(endX, endY / 6, endX / 1.05, endY / 3, endX, endY);
+      ctx.stroke();
+    }
+  });
+
   if (!currentNode) return;
 
   const treeNode = css`
     display: flex;
     flex-direction: column;
     padding: 1rem;
-    position: relative;
   `;
 
   const nodeContent = css`
@@ -27,6 +66,7 @@ export default function TreeNode(props) {
     width: 180px;
     text-align: center;
     min-height: 100px;
+    position: relative;
   `;
 
   const treeLevel = css`
@@ -35,16 +75,27 @@ export default function TreeNode(props) {
     justify-content: center;
   `;
 
-  const treeBranch = css`
-    top: 100px;
+  const nodeBranch = css``;
+
+  const nodeChild = css`
+    position: relative;
+    padding: 0;
+  `;
+
+  const canvasStyle = css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
   `;
 
   const nextNode = currentNode?.next ? (
-    <TreeNode nodeData={nodeData} curNode={currentNode.next} />
+    <div className={nodeChild} ref={nodeChildRef}>
+      <TreeNode nodeData={nodeData} curNode={currentNode.next} />
+    </div>
   ) : null;
 
   const branchNode = currentNode?.branch ? (
-    <div className={treeBranch}>
+    <div className={nodeBranch} ref={nodeBranchRef}>
       <TreeNode nodeData={nodeData} curNode={currentNode.branch} />
     </div>
   ) : null;
@@ -60,13 +111,19 @@ export default function TreeNode(props) {
   return (
     <>
       <div className={treeNode}>
-        <div className={nodeContent}>
+        <div className={nodeContent} ref={currentNodeRef}>
           <p>{currentNode?.value}</p>
           {treeNode && branchNode ? null : (
             <button onClick={openAddChildForm}>
               <AddIcon fontSize="small" />
             </button>
           )}
+          <canvas
+            className={canvasStyle}
+            ref={canvasRef}
+            width="1000"
+            height="100"
+          />
         </div>
         <div className={treeLevel}>
           {nextNode}
--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,11 @@ import styles from "@/styles/Home.module.css";
 import Tree from "@/components/Tree/Tree";
 import { NodeProvider } from "@/testdata";
 import { useEffect } from "react";
+import { css } from "@emotion/css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  useEffect(() => {
-    const canvas = document.getElementById("tutorial");
-    const ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = "rgb(200, 0, 0)";
-    ctx.fillRect(10, 10, 50, 50);
-
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect(30, 30, 50, 50);
-  }, []);
-
   return (
     <>
       <Head>
@@ -32,9 +22,6 @@ export default function Home() {
           <Tree />
         </main>
       </NodeProvider>
-      <canvas id="tutorial" width="150" height="150">
-        current stock price: $3.15 + 0.15
-      </canvas>
     </>
   );
 }
